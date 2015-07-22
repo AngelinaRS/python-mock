cd2e8ae6dba327c8cadcf054f7e2acf481d34cb3
AngelinaRS <candy26y_@hotmail.com>
2015-07-22 08:58:19 -0600

    Test to remove

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import application
 import mock
 import unittest
@@ -7,9 +10,32 @@ class Test_application(unittest.TestCase):
 	@mock.patch('application.os.path')
 	@mock.patch('__builtin__.open')
 
-	def test_exists_file(self, mock_open, mock_path):
+	def test_exists_file_create(self, mock_open, mock_path):
 		mock_path.isfile.return_value == True
-		application.create("File exists")
-		mock_open.assert_called_with("File exists.txt", "w")
+		self.assertFalse(mock_open.called, "File exists, can't create it")
 
+	@mock.patch('application.os.path')
+	@mock.patch('__builtin__.open')
 
+	def test_not_exist_file_create(self, mock_open, mock_path):
+		mock_path.isfile.return_value == False
+		application.create("file_name")
+		mock_open.assert_called_with("file_name", "w")
+
+	@mock.patch('application.os.path')
+	@mock.patch('application.os')
+
+	def test_exists_file_remove(self, mock_os, mock_path):
+		mock_path.isfile.return_value = True
+		application.remove("file_name")
+		mock_os.remove.assert_called_with("file_name")
+
+	@mock.patch('application.os.path')
+	@mock.patch('application.os')
+
+	def test_not_exist_file_remove(self, mock_os, mock_path):
+		mock_path.isfile.return_value = False
+		self.assertFalse(mock_os.remove.called, "Can't create the file")
+
+if __name__ =="__main":
+	unittest.main()
